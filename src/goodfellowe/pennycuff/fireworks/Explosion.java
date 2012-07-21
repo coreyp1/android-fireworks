c755fdb57dcaebf5ec44218b250362fd40df22eb
Corey Pennycuff <pennycuff.c@gmail.com>
2012-07-20 19:51:13 -0500

    Further abstracting of Explosion constructor, added Explosion Random class

--- src/goodfellowe/pennycuff/fireworks/Explosion.java
+++ src/goodfellowe/pennycuff/fireworks/Explosion.java
@@ -29,18 +29,7 @@ public class Explosion {
 	 */
 	public Explosion(int x, int y, int screenHeight) {
 		this.screenHeight = screenHeight;
-		Random random = new Random();
-		numParticles = 500;
-		particles = new Explosion.Particle[numParticles];
-		int radius = 5;
-		double radians;
-		for (int i = 0; i < numParticles; i++) {
-			radians = random.nextDouble() * 2 * Math.PI;
-			particles[i] = new Explosion.Particle(x,  y, random.nextInt(Ember.LIGHTS_TOTAL), screenHeight);
-			particles[i].setGravity(0, -2);
-			particles[i].velocityX = Math.cos(radians) * radius * random.nextDouble();
-			particles[i].velocityY = Math.sin(radians) * radius * random.nextDouble();
-		}
+		particles = new Explosion.Particle[0];
 	}
 	
 	public boolean draw(Canvas canvas, long currentTime) {
---
+++ src/goodfellowe/pennycuff/fireworks/ExplosionRandom.java
@@ -0,0 +1,34 @@
+/**
+ * 
+ */
+package goodfellowe.pennycuff.fireworks;
+
+import java.util.Random;
+
+/**
+ * @author Corey Pennycuff and Rob Goodfellowe
+ *
+ */
+public class ExplosionRandom extends Explosion {
+
+	/**
+	 * @param x
+	 * @param y
+	 * @param screenHeight
+	 */
+	public ExplosionRandom(int x, int y, int screenHeight) {
+		super(x, y, screenHeight);
+		Random random = new Random();
+		numParticles = 75;
+		particles = new Explosion.Particle[numParticles];
+		int radius = 5;
+		double radians;
+		for (int i = 0; i < numParticles; i++) {
+			radians = random.nextDouble() * 2 * Math.PI;
+			particles[i] = new Explosion.Particle(x,  y, random.nextInt(Ember.LIGHTS_TOTAL), screenHeight);
+			particles[i].setGravity(0, -2);
+			particles[i].velocityX = Math.cos(radians) * radius * random.nextDouble();
+			particles[i].velocityY = Math.sin(radians) * radius * random.nextDouble();
+		}
+	}
+}
