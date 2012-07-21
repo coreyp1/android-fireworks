e7fe38db0beab5271d7228c1bfa4d64de763dba7
Corey Pennycuff <pennycuff.c@gmail.com>
2012-07-21 11:59:17 -0500

    making lights slightly smaller

--- src/goodfellowe/pennycuff/fireworks/Ember.java
+++ src/goodfellowe/pennycuff/fireworks/Ember.java
@@ -28,7 +28,7 @@ public class Ember {
 	static final public boolean DEAD = false;
 	static final public int LIGHTS_ACROSS = 20;
 	static final public int LIGHTS_TOTAL = LIGHTS_ACROSS * LIGHTS_ACROSS;
-	static final public int LIGHT_SIZE = 10;
+	static final public int LIGHT_SIZE = 8;
 	static final public int LIGHT_MASTER_SIZE = LIGHT_SIZE * LIGHTS_ACROSS;
 	static final public int LIGHT_SIZE_OFFSET = LIGHT_SIZE / 2;
 	static final public float LIGHT_ALPHA = (float) .8;
